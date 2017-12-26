1afdfe87436c65e71763d52a0ffa59977d62ba48
BorisMR <anum.bmr.01@gmail.com>
2017-12-26 10:39:13 +0000

    indica cuando no encuentra envios

--- resources/views/listEnviosGuest.blade.php
+++ resources/views/listEnviosGuest.blade.php
@@ -8,35 +8,38 @@
                     <div class="panel-heading">
                         Estado de Envio
                     </div>
-
                     <div class="panel-body">
                         <table class="table">
+                        @if( count($envios) === 0 )
+                                <td><b>No se encontró ningún envío con ese ID</b></td>
+                        @else
                             <tr>
                                 <th>ID</th>
                                 <th>Estado</th>
                                 <th>Fecha de Creación</th>
                             </tr>
                             @foreach($envios as $envio)
-                            <tr>
-                                <td>{{ $envio['id'] }}</td>
-                                <td>
-                                    @if (($envio->id_status) == 0)
-                                        Registrado en sistema
-                                    @elseif (($envio->id_status) == 1)
-                                        Despachado
-                                    @elseif (($envio->id_status) == 2)
-                                        En transito
-                                    @elseif (($envio->id_status) == 3)
-                                        En entrega
-                                    @elseif (($envio->id_status) == 4)
-                                        Entrega confirmada
-                                    @else
-                                        INDETERMINADO
-                                    @endif
-                                </td>
-                                <td>{{ $envio['created_at'] }}</td>
-                            </tr>
+                                <tr>
+                                    <td>{{ $envio['id'] }}</td>
+                                    <td>
+                                        @if (($envio->id_status) == 0)
+                                            Registrado en sistema
+                                        @elseif (($envio->id_status) == 1)
+                                            Despachado
+                                        @elseif (($envio->id_status) == 2)
+                                            En transito
+                                        @elseif (($envio->id_status) == 3)
+                                            En entrega
+                                        @elseif (($envio->id_status) == 4)
+                                            Entrega confirmada
+                                        @else
+                                            INDETERMINADO
+                                        @endif
+                                    </td>
+                                    <td>{{ $envio['created_at'] }}</td>
+                                </tr>
                             @endforeach
+                        @endif
                         </table>
                     </div>
                 </div>
